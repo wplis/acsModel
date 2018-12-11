b215e50b41cab4a5480c77fedb495beb6b8dbade
wplis <wplis@ukr.net>
2018-12-11 12:06:34 +0200

    start acs project
    create main class

---
+++ acsmodel.cpp
@@ -0,0 +1,6 @@
+#include "acsmodel.h"
+
+
+Acsmodel::Acsmodel()
+{
+}
---
+++ acsmodel.h
@@ -0,0 +1,12 @@
+#ifndef ACSMODEL_H
+#define ACSMODEL_H
+
+
+class Acsmodel
+{
+
+public:
+    Acsmodel();
+};
+
+#endif // ACSMODEL_H
