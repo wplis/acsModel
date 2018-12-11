32e26240aff9f5dd62f2de57b953eede9c445e22
wplis <wplis@ukr.net>
2018-12-11 17:48:54 +0200

    universalModel

--- acsmodel.cpp
+++ acsmodel.cpp
@@ -1,6 +1,77 @@
 #include "acsmodel.h"
 
-
-Acsmodel::Acsmodel()
+acsModel::acsModel(btc newId)
 {
+    id = newId;
+
+    for(i=0; i<16; i++)
+        sensors[i] = 255;
+}
+
+btc acsModel::shId()
+{
+    return id;
+}
+
+btc acsModel::setAddr(btc newAddr)
+{
+    addr = newAddr;
+    return addr;
+}
+
+btc acsModel::calcPing(btc operation, btc first, btc second)
+{
+    btc answer;
+
+    if(second > first)
+    {
+        answer = first;
+        first = second;
+        second = answer;
+    }
+
+    switch(operation)
+    {
+        case 0:
+            answer = first + second;
+        break;
+        case 1:
+            answer = first - second;
+        break;
+        case 2:
+            answer = first * second;
+        break;
+        case 3:
+            answer = first / second;
+        break;
+        default:
+            answer = 255;
+    }
+    return answer;
+}
+
+btc acsModel::sensor(btc number)
+{
+    return sensors[number];
+}
+
+btc acsModel::addSensor(btc pin)
+{
+    for(i=0; i<16 && sensors[i]<255; i++);
+    if(i>15)
+        return 255;
+    sensors[i] = pin;
+
+    return i;
+}
+
+btc acsModel::delSensor(btc number)
+{
+    if(number > 15)
+        return 255;
+    for(i=number; i<15; i++)
+        sensors[i] = sensors[i+1];
+    sensors[i] = 255;
+
+    return i;
 }
--- acsmodel.h
+++ acsmodel.h
@@ -1,12 +1,25 @@
 #ifndef ACSMODEL_H
 #define ACSMODEL_H
 
+#define btc unsigned char
 
-class Acsmodel
+class acsModel
 {
+private:
+    btc addr;
+    btc id;
+    btc i;
+    btc sensors[16];
 
 public:
-    Acsmodel();
+    acsModel(btc);
+
+    btc shId();
+    btc setAddr(btc);
+    btc calcPing(btc, btc, btc);
+    btc sensor(btc);
+    btc addSensor(btc);
+    btc delSensor(btc);
 };
 
 #endif // ACSMODEL_H
